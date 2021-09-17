538ae1b2506acd76ed4628c7d26361db8e3074ba
Everton-ZUP <everton.fabris@zup.com.br>
2021-09-16 21:18:06 -0300

    Login via senha do cartao

---
+++ src/main/java/br/com/zupacademy/propostas/seguranca/ConfiguracaoSeguranca.java
@@ -0,0 +1,27 @@
+package br.com.zupacademy.propostas.seguranca;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@Configuration
+@EnableWebSecurity
+@Profile("dev")
+public class ConfiguracaoSeguranca extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http    .cors().and().csrf().disable()
+                .authorizeRequests(authorizeRequest ->
+                        authorizeRequest
+                                .antMatchers("/**").hasAuthority("SCOPE_Teste-Scope")
+                                .anyRequest().authenticated()
+        ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
